66aa1bfd0146f2e76055a3cdc5d780e607e16be7
TonioBay <sanchezb.antonio10@gmail.com>
2025-03-12 16:34:23 -0600

    Python file
    Script para extracción de datos

---
+++ main.py
@@ -0,0 +1,35 @@
+import tabula
+import pandas as pd
+
+tables = tabula.read_pdf(
+    r"C:\Users\Max_1\Downloads\ano-2024.pdf",
+    pages = '2-3',
+    multiple_tables = True,
+    lattice= True,
+    stream = True,
+    guess = False,
+    pandas_options= {"header": None}
+)
+ 
+df = tables[1].dropna(axis = 0, how = 'all')
+df = df.drop(range(0,2))
+n = 1
+columns = [0]
+for i in range(8):
+    if n < 7:
+        df[[f'{n}.2',f'{n}.3']] =df[n].str.split(r'[ \r]', n = 1, expand = True)
+    else:
+        df[f'{n}.2'] = df[n] 
+        df[f'{n}.3'] = 'CEDA'
+    if n % 2 == 1 :
+        tipo_costo = 'Bajo'
+    else:
+        tipo_costo = 'alto'
+    df[f'{n}.1'] = tipo_costo
+    columns.extend([f'{n}.1',f'{n}.2',f'{n}.3'])
+    n += 1
+df = df.drop(df.columns[1:n], axis = 1)
+#columns.extend([9])
+df = df[columns]
+
+print(df.columns)
